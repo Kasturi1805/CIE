d334ab238daddbb823dae027991441df993b3076
Kasturi1805 <swapnagokul8@gmail.com>
2025-04-15 09:50:46 -0500

    This is my java file

---
+++ Check_eligiblity.java
@@ -0,0 +1,18 @@
+package CIE;
+
+public class Check_eligiblity {
+    public static void main(String[] args) {
+        int age = 19;
+
+        if(age<18)
+        {
+            System.out.println("Not Eligible for vote");
+        }
+
+        else
+        {
+            System.out.println("Eligible for vote");
+        }
+    }
+    
+}
